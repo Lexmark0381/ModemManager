51751c98536c3feff94bba31fb0b8a3656acbf28
Piermarco Barbè <barbep@usi.ch>
2017-04-25 16:04:23 +0200

    nano indentation still sucks

--- gpio.py
+++ gpio.py
@@ -3,17 +3,19 @@ import RPi.GPIO as GPIO, time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
+
 def on():
 	GPIO.output(18, 1)
+
 def off():
 	GPIO.output(18, 0)
+
 def reboot(t):
 	try:
 		t = int(t)
 	except:
 		print(t + " is not a valid timeout")
 		return
-        GPIO.output(18, 0)
+    GPIO.output(18, 0)
 	time.sleep(t)
-        GPIO.output(18, 1)
-
+    GPIO.output(18, 1)
