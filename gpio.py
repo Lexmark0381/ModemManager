13d72360c9cff32a1599e1769cfab998b1044ac2
Piermarco Barbè <barbep@usi.ch>
2017-04-25 16:08:43 +0200

    nano plz

--- gpio.py
+++ gpio.py
@@ -6,16 +6,19 @@ GPIO.setup(18, GPIO.OUT)
 
 def on():
 	GPIO.output(18, 1)
+	return 0
 
 def off():
 	GPIO.output(18, 0)
+	return 0
 
 def reboot(t):
 	try:
 		t = int(t)
 	except:
 		print(t + " is not a valid timeout")
-		return
-    GPIO.output(18, 0)
+		return -1
+	GPIO.output(18, 0)
 	time.sleep(t)
-    GPIO.output(18, 1)
+	GPIO.output(18, 1)
+	return 0
